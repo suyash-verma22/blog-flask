7548aaff8b268b5761dccc08f1e6885c00fdf25b
Suyash-verma22 <verma.suaysh22@gmail.com>
2020-08-27 00:29:07 +0530

    database created in app.py

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
 
+#connecting to database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy(app)
+
+# localhost:5000/user/suyash/17103052 type url will work
 @app.route('/user/<string:name>/<int:id>/')  
 def print_hi(name,id):
     return "Hello, " + name + " Welcome " + "id: " + str(id); 
-all_posts = [
+
+"""all_posts = [
     {
     'title': 'Post 1',
     'content': 'This is my content of post 1',
@@ -16,7 +24,9 @@ all_posts = [
     'title': 'Post 2',
     'content': 'This is my content of post 2',
     }
-]
+]"""
+
+# localhost:5000/posts will work
 @app.route('/posts')
 def show_posts():
     return render_template('posts.html',posts = all_posts)
